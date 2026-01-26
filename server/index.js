7d3b7b508ae4c10fc17662dc14ccab45e63f597f
Drei Gaming YT <jamesjuliano90@gmail.com>
2026-01-26 16:14:51 +0800

    exposing

--- server/index.js
+++ server/index.js
@@ -903,4 +903,4 @@ app.get('/api/events', authMiddleware, async (req,res)=>{
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('API + static server listening on', PORT));
+app.listen(PORT, '0.0.0.0', () => console.log('API + static server listening on', PORT));
